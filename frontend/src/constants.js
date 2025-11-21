2f73107d674c4ee812689cefe1ff7da7fc366057
a3ela <abelsintayehu626@gmail.com>
2025-11-21 16:25:24 +0300

    update: to be able to apply redux

---
+++ frontend/src/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = 'http://localhost:5000';
+export const AUTH_URL = '/api/auth';
+export const USERS_URL = '/api/users';
+export const CHATS_URL = '/api/chats';
+export const SESSIONS_URL = '/api/sessions';
